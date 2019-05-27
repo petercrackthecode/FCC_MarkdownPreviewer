db97e7c27f9623e2abd70c1f8f5a436fcb27b601
Peter Nguyen <nguyenvandung02051999@gmail.com>
2019-05-26 22:32:09 -0700

    Create a bunch of stuff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import Preview from 'components/Preview.js';
+import Editor from 'components/Editor.js';
+import Header from 'components/Header.js';
+
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  state= {
+    m_text: 'Start creating your beautiful article by writing on the Preview Area',
+  };
+
+  handleTextChange= (text) =>  {
+    this.setState({m_text: text});
+  }
+
+  render()  {
+    return (
+      <div className="App">
+        <Header/>
+        <Preview text={this.state.m_text}/>
+        <Editor text={this.state.m_text} onTextChange={this.handleTextChange}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Preview.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import marked from 'marked'
+
+export default function Preview()   {
+    return (
+        <div id='preview'>
+
+        </div>
+    );
+}
